b434b618d38476c08a345d2366b1bdcf7466101d
live <live@live.com>
2022-10-11 14:24:28 +0800

    开发商城

---
+++ app/admin/shop.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\admin;
+
+class shop
+{
+    public function list()
+    {
+        return ['shop'];
+    }
+
+    public function add()
+    {
+        return ['add'];
+    }
+
+}
